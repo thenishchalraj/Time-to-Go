d2cd060359356d64820cdbe619b338a8a7128814
mishra3452 <mishrasumit.3452@gmail.com>
2018-11-08 23:26:15 +0530

    add one more type of for

--- 6loopInGo.go
+++ 6loopInGo.go
@@ -1,10 +1,10 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	//the only type of loop in Go is for loop
 	for i := 0; i < 6; i++ { //everything is same, just remove the brackets
 		fmt.Println(i)
@@ -12,8 +12,14 @@ func main(){
 
 	//for loop also works as the while loop
 	j := 0
-	for j < 6{
+	for j < 6 {
 		fmt.Println(j) //so this will return the same numbers as the above loop
 		j++
+	}
+
+	//for loop can be used without having any exit condition. will come out of for by 'break'
+	for {
+		fmt.Println("in for")
+		break
 	}
 }
